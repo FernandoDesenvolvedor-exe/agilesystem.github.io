920e909512e99ed1cc5e4c21020c187a233ba036
Luis Fernando Pereira <110871690+FernandoDesenvolvedor-exe@users.noreply.github.com>
2024-07-05 15:36:19 -0300

    atualização site

--- app/dist/js/script.js
+++ app/dist/js/script.js
@@ -1,6 +1,7 @@
 const menu = document.querySelector('#toggle-btn');
 const main = document.getElementById('principal');
 const dropdown = document.getElementById('menu-dropdown');
+const janela = window.innerWidth;
 
 if(sessionStorage.getItem('menuOpen') == 'true'){
     document.querySelector("#sidebar").classList.add("expand");    
@@ -22,16 +23,20 @@ menu.addEventListener("click", function(event){
 
     document.querySelector("#sidebar").classList.toggle("expand");
     
-    if(document.querySelector('#sidebar').classList.contains("expand")){
-        main.style.left = '15%';
-        main.style.width = '85%';
-        sessionStorage.setItem('menuOpen', true);
-    } else {
-        main.style.left = '4.8%';
-        main.style.width = '95%';
-        sessionStorage.setItem('menuOpen', false);
-    }  
-        
+    if(janela > 576){
+        if(document.querySelector('#sidebar').classList.contains("expand")){
+            
+
+            
+            main.style.left = '20%';
+            main.style.width = '80%';
+            sessionStorage.setItem('menuOpen', true);
+        } else {
+            main.style.left = '4.8%';
+            main.style.width = '95%';
+            sessionStorage.setItem('menuOpen', false);
+        }  
+    } 
 });
 
 function ativaItem(element){    
